9045f3b96448a72b5677f791b4fb937c1eb4afa4
Lukas Walther <lukas100@cs.au.dk>
2013-12-10 11:25:13 +0100

    Implemented Hood and Melville O(1) Queues.

--- src/haskell/queues.hs
+++ src/haskell/queues.hs
@@ -13,8 +13,10 @@ class Queue q where
 	insert :: a -> q a -> q a
 	remove :: q a -> ( a, q a )
 	make :: [a] -> q a 
-	make = foldr insert empty
-	
+	make = foldr insert empty -- default implementation
+	peek :: q a -> a
+	peek q = fst $ remove q -- default implementation
+
 -- queues from lists
 instance Queue [] where
 	empty = []
@@ -26,6 +28,7 @@ instance Queue [] where
 		let x:xs' = xs in
 		(x, xs')
 	make xs = xs
+	peek xs = head xs
 
 -- queues from simple pairs
 	-- this defines a constructor for a pair of lists, and is used to disambiguate other pairs of lists.
@@ -42,13 +45,9 @@ instance Queue PairOfList where
 	empty = POL ([],[])
 	size (POL (xs, ys)) = length xs + length ys
 	insert x (POL (xs, ys)) = (POL ( xs, x:ys))
-	remove (POL (xs,ys)) = 
-		if size xs > 0 then
-			(head xs, (POL (tail xs, ys)))
-		else
-			remove (POL (reverse ys, []))
-	make xs = POL ([],xs)
-
+	remove (POL (xs,ys))
+		| size xs > 0 = (head xs, (POL (tail xs, ys)))
+		| otherwise   = remove (POL (reverse ys, []))
 -- okasaki queue.
 newtype TripleOfList a = TOL { unTOL :: ([a],[a],[a]) }
 
@@ -73,7 +72,7 @@ instance Queue TripleOfList where
 	insert x (TOL (ls, rs, ts)) = makeq (TOL (ls, x:rs, ts))
 	remove (TOL (ls, rs, ts)) = (head ls, makeq (TOL (tail ls, rs, ts)))
 
--- constant time worst case queues.
+-- constant time worst case queues. !!! These are apparently incorrect !!!
 newtype RealTimePair a = RTP { unRT :: (Int, Int, [a], [a], [a], [a], [a], [a]) }
 
 f:: RealTimePair a -> RealTimePair a
@@ -89,10 +88,6 @@ f(RTP(d, s, fs, as, [], [], ds, es)) =
 instance Queue RealTimePair where
 	empty = RTP (0,0, [], [], [], [], [], [])
 	size (RTP (d, s, fs, as, bs, cs, ds, es)) = length fs + length cs + length ds + length es 
-	make xs = case xs of
-		[] -> empty
-		[x] -> RTP (0, 0, [x], [], [x], [], [], [])
-		(x:xs') -> insert x $ make xs'
 	insert x (RTP (d, s, fs, as, bs, cs, ds, es)) = f(f(f(RTP(d, s, fs, as, bs, cs, ds, x:es))))
 	remove (RTP (d, s, fs, as, bs, cs , ds, es)) =
 		if d == s && length fs == 0 && length bs == 0 && length cs == 0 then
@@ -101,3 +96,35 @@ instance Queue RealTimePair where
 		else
 			let x:fs' = fs in
 			(x, f(f(f(f(RTP( d+1, s, fs', as, bs, cs, ds, es))))))
+
+-- Real time queue 2, paper : http://ecommons.library.cornell.edu/handle/1813/6273
+-- 	Real Time Queue Operations in Pure LISP
+
+-- we implement size in constant time
+
+newtype RealTimePair2 a = RTP2 { unRTP2 :: (Bool, Int, Int, [a], [a], [a], [a], [a], [a], Int) }
+
+onestep :: RealTimePair2 a -> RealTimePair2 a
+onestep q
+	| not recopy = q
+	| recopy && length hS > 0 && length tS > 0  =
+		RTP2 (True, lendiff+1, nrcopy+1, tail hS, tail tS, hs, head tS : hS', tS', head hS : hr, size)
+	| recopy && length hS == 0 && length tS > 0 =
+		RTP2 (True, lendiff+1, nrcopy, [], [], hs, head tS : hS', tS', hr, size)
+	| recopy && length hS == 0 && length tS == 0 && nrcopy > 1 =
+		RTP2 (True, lendiff+1, nrcopy-1, [], [], hs, head hr : hS', tS', tail hr, size)
+	| recopy && length hS == 0 && length tS == 0 && nrcopy == 1 =
+		RTP2 (False, lendiff+1, 0, head hr : hS', tS', [], [], [], [], size)
+	where RTP2(recopy, lendiff, nrcopy, hS, tS, hs, hS', tS', hr, size) = q
+
+instance Queue RealTimePair2 where
+	empty = RTP2 (False, 0, 0, [], [], [], [], [], [], 0)
+	insert v (RTP2( recopy, lendiff, nrcopy, hS, tS, hs, hS', tS', hr, size))
+		| not recopy && lendiff > 0  = RTP2( False, lendiff-1, 0, hS, v:tS, [], [], [], [], size+1)
+		| not recopy && lendiff == 0 = onestep( onestep( RTP2 (True, 0, 0, hS, v:tS, hS, [], [], [], size+1)))
+		| recopy 		     = onestep( onestep( RTP2 (True, lendiff -1, nrcopy, hS, tS, hs, hS', v:tS, hr, size+1)))
+	remove (RTP2( recopy, lendiff, nrcopy, hS, tS, hs, hS', tS', hr, size))
+		| not recopy && lendiff > 0  = ( head hS, RTP2 (False, lendiff-1, 0, tail hS, tS, [], [], [], [], size-1))
+		| not recopy && lendiff == 0 = ( head hS, onestep $ onestep (RTP2 (True, 0, 0, tail hS, tS, tail hS, [], [], [], size-1)))
+		| recopy		     = ( head hs, onestep $ onestep (RTP2 (True, lendiff-1,nrcopy, hS, tS, tail hs, hS', tS', hr, size-1)))
+	size (RTP2( _, _, _, _, _, _, _, _, _, size')) = size'
