c1b5b230eaf1201171f3f61b2720354d1a707b87
Lukas Walther <lukas100@cs.au.dk>
2013-12-10 09:55:41 +0100

    Implemented Queues. May have error.

--- src/haskell/queues.hs
+++ src/haskell/queues.hs
@@ -74,4 +74,30 @@ instance Queue TripleOfList where
 	remove (TOL (ls, rs, ts)) = (head ls, makeq (TOL (tail ls, rs, ts)))
 
 -- constant time worst case queues.
-newtype RealTime a = RT { unRT :: (Int,Int,[a],[a],[a],[a],[a]) }
+newtype RealTimePair a = RTP { unRT :: (Int, Int, [a], [a], [a], [a], [a], [a]) }
+
+f:: RealTimePair a -> RealTimePair a
+f(RTP(d, 0, fs, [], bs, x:cs, ds, es)) = RTP(d, 0 ,fs, [], bs, cs, x:ds, es)
+f(RTP(d, s, fs, as, x:bs, [], ds, es)) = RTP(d, s+1, fs, x:as, bs, [], ds, es)
+f(RTP(d, s, fs, as, [], [], ds, es)) =
+	if s > d then
+		let x:as' = as in
+		RTP(d, s-1, fs, as', [], [], x:ds, es)
+	else
+ 		RTP(0, 0, ds, [], ds, es, [], [])
+
+instance Queue RealTimePair where
+	empty = RTP (0,0, [], [], [], [], [], [])
+	size (RTP (d, s, fs, as, bs, cs, ds, es)) = length fs + length cs + length ds + length es 
+	make xs = case xs of
+		[] -> empty
+		[x] -> RTP (0, 0, [x], [], [x], [], [], [])
+		(x:xs') -> insert x $ make xs'
+	insert x (RTP (d, s, fs, as, bs, cs, ds, es)) = f(f(f(RTP(d, s, fs, as, bs, cs, ds, x:es))))
+	remove (RTP (d, s, fs, as, bs, cs , ds, es)) =
+		if d == s && length fs == 0 && length bs == 0 && length cs == 0 then
+			let x:ds' = ds in
+			(x,f(f(f(f(RTP(0, 0, ds', [], ds', es, [], []))))))
+		else
+			let x:fs' = fs in
+			(x, f(f(f(f(RTP( d+1, s, fs', as, bs, cs, ds, es))))))
