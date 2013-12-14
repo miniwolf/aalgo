c2bf57c4113ac67f60f131418f2acc3d569dc880
lukas100 <lukas100@cs.au.dk>
2013-12-14 16:39:53 +0100

    Progress on how to performance test.

--- src/haskell/performance.hs
+++ src/haskell/performance.hs
@@ -4,4 +4,15 @@ import Queues
 
 main :: IO ()
 main = do
-	putStrLn("empty queues: " ++ (show $emptyQueues))
+	--putStrLn("empty queues: " ++ (show $emptyQueues))
+	args <- getArgs
+	if length args == 2
+	then
+		let (q1,q2,q3,q4) = emptyQueues in case args of
+			[nstring,queue] -> case queue of 
+				"List" -> let q = foldr insert [n,n-1..1] q1 in putStrLn ( (show $ peek q)	++ ", "++ (show $ size q))
+				where n = read(nstring)	
+			_ -> putStrLn "Something is wrong :/"
+			
+	else
+		putStrLn "Not enough arguments, need 2"
