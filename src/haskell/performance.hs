6b4d2701182ee6d4af8ab6b0d9017e0c77abcbb3
lukas100 <lukas100@cs.au.dk>
2014-01-03 17:25:42 +0100

    Changed function repetition.
    The framework does repetition for us,
    we only need to perform the same operations.

--- src/haskell/performance.hs
+++ src/haskell/performance.hs
@@ -3,49 +3,48 @@ import System.Environment
 import Criterion.Main
 import Queues
 
-main =  
-  let min = 128 
-      max = 4096 
+main =
+  let min = 128
+      max = 4096
   in
    defaultMain [bgroup "insert-list" $ generateTest performPeekList min max
                ,bgroup "insert-pair" $ generateTest performPeekPair min max
                ,bgroup "insert-triple" $ generateTest performPeekTriple min max
                ,bgroup "insert-const" $ generateTest performPeekConst min max
-               ,bgroup "insert-list-repeat" $ generateTestDel performInsertList performPeekInsertRepeat min max
-               ,bgroup "insert-pair-repeat" $ generateTestDel performInsertPair performPeekInsertRepeat min max
-               ,bgroup "insert-triple-repeat" $ generateTestDel performInsertTriple performPeekInsertRepeat min max
-               ,bgroup "insert-const-repeat" $ generateTestDel  performInsertConst performPeekInsertRepeat min max
+               ,bgroup "insert-list-repeat" $ generateTestDel performInsertList performIns min max
+               ,bgroup "insert-pair-repeat" $ generateTestDel performInsertPair performIns min max
+               ,bgroup "insert-triple-repeat" $ generateTestDel performInsertTriple performIns min max
+               ,bgroup "insert-const-repeat" $ generateTestDel  performInsertConst performIns min max
                ,bgroup "delete-list" $ generateTestDel performInsertList performPeekDel min max
                ,bgroup "delete-pair" $ generateTestDel performInsertPair performPeekDel min max
                ,bgroup "delete-triple" $ generateTestDel performInsertTriple performPeekDel min max
                ,bgroup "delete-const" $ generateTestDel performInsertConst performPeekDel min max
-               ,bgroup "delete-list-repeat" $ generateTestDel performInsertList performPeekDelRepeat min max
-               ,bgroup "delete-pair-repeat" $ generateTestDel performInsertPair performPeekDelRepeat min max
-               ,bgroup "delete-triple-repeat" $ generateTestDel performInsertTriple performPeekDelRepeat min max
-               ,bgroup "delete-const-repeat" $ generateTestDel performInsertConst performPeekDelRepeat min max]
+               ,bgroup "delete-list-repeat" $ generateTestDel performInsertList performDel min max
+               ,bgroup "delete-pair-repeat" $ generateTestDel performInsertPair performDel min max
+               ,bgroup "delete-triple-repeat" $ generateTestDel performInsertTriple performDel min max
+               ,bgroup "delete-const-repeat" $ generateTestDel performInsertConst performDel min max]
 
-performInsertList = 
+performInsertList =
   let (q1,q2,q3,q4) = emptyQueues in
   performInsert q1
-  
-performInsertPair = 
+
+performInsertPair =
   let (q1,q2,q3,q4) = emptyQueues in
   performInsert q2
- 
-performInsertTriple = 
+
+performInsertTriple =
   let (q1,q2,q3,q4) = emptyQueues in
   performInsert q3
-  
-performInsertConst = 
+
+performInsertConst =
   let (q1,q2,q3,q4) = emptyQueues in
   performInsert q4
-  
 
-performInsert q n = 
+performInsert q n =
   foldr insert q [n, n-1..1]
- 
-performPeek qg n = 
-  let q = qg n in 
+
+performPeek qg n =
+  let q = qg n in
   case peek q of Just x -> x
                  Nothing -> 0
 
@@ -53,7 +52,7 @@ performPeekList = performPeek performInsertList
 
 performPeekPair = performPeek performInsertPair
 
-performPeekTriple = performPeek performInsertTriple 
+performPeekTriple = performPeek performInsertTriple
 
 performPeekConst = performPeek performInsertConst
 
@@ -63,14 +62,16 @@ performPeekDelRepeat qe = performPeek (\ n -> foldr (\ _ _ -> remove qe ) qe [n,
 
 performPeekInsertRepeat qe  = performPeek (\ n -> foldr (\ _ _ -> insert (n+1) qe ) qe [n, n-1..1])
 
+performDel qe = remove qe
+
+performIns qe = insert 1 qe
 
 generateTest pfk min max
 	| min >= max = [bench ("list-"++show min) $ nf pfk min]
 	| otherwise  = (bench ("list-"++show min) $ nf pfk min) : (generateTest pfk (min*2) max)
 
-
 generateTestDel :: (Int -> a) -> (a -> Int -> Int) -> Int -> Int -> [Benchmark]
-generateTestDel qg pfk min max = 
-  let qe = qg $! min in  -- $! should force the function to strict evaluation instead of lazy 
+generateTestDel qg pfk min max =
+  let qe = qg $! min in  -- $! should force the function to strict evaluation instead of lazy
   case min >= max of True -> [bench ("list-"++show min) $ nf (pfk qe) min]
                      False -> (bench ("list-"++show min) $ nf (pfk qe) min) : (generateTestDel qg pfk (min*2) max)
